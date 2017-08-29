88d54a01b2f4cec91dd19b2b07f9bf89d1f0170e
Mike Moran <pymike42@gmail.com>
2017-08-28 21:34:38 -0400

    Updates

--- publish.sh
+++ publish.sh
@@ -32,7 +32,7 @@ git commit -m "Updates"
 git checkout -B gh-pages
 cp -r /tmp/workspace/* .
 git add -A .
-git commit -m $@
+git commit -m "Updates"
 git push origin master gh-pages
 git checkout master
 rm -rf /tmp/workspace
